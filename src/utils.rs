d6cfd48d164639dc1110512aee9ce307ce3e1142
Devashish Dalvi <devashish.dalvi99@gmail.com>
2025-06-15 22:52:43 +0530

    [C] 1.3. Start of Parsing Logic
    - Creating logic for parsing.
    - Beginning of Logic
    - Test Fails
    =======[20250615]=======

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,17 @@
 pub(crate) fn extract_digits(s: &str) -> (&str, &str){
-    (&s[1..], &s[0..1])
+    let mut digit_end = 0;
+    
+    for (idx, c) in s.char_indices() {
+        if c.is_ascii_digit() {
+            digit_end = idx + 1;
+        } else { 
+            break;
+        }
+    }
+    
+    let digits = &s[..digit_end];
+    let remainder = &s[digit_end..];
+    (digits, remainder)
 }
 
 #[cfg(test)]
@@ -7,7 +19,12 @@ mod tests {
     use super::*;
     
     #[test]
-    fn extract_digits_test() {
+    fn extract_one_digits_test() {
         assert_eq!(extract_digits("1+2"), ("+2", "1"));
+    }
+    
+    #[test]
+    fn extract_multiple_digits_test() {
+        assert_eq!(extract_digits("10-20"), ("-20", "10"));
     }
 }
